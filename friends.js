5bcfbb47e5523dfb8f28efd917713ed8d8e3c9ba
Prissilla Escobar <prissilla988@gmail.com>
2023-04-20 17:56:33 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,12 @@
+var array = [10, 20, 30, 40, 50, 60, 70, 80]
+for (var i = 0; i < array.length; i++) {
+    if (array[i] > 30) {
+        console.log(array[i])
+    }
+}
+
+for (var i = 0; i < array.length; i++) {
+    if (array[i] < 60) {
+        console.log(array[i])
+    }
+}
